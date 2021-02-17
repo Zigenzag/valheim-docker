7a205fef87670ab22360821231cc485b9b82ae42
Bradley Shaw <bradley.shaw@unit27.co.uk>
2021-02-17 02:49:01 +0000

    Progress made on dockerfiles

---
+++ .docker/valheim/run_valheim_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export temp_ldpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/valheim/linux64:$LD_LIBRARY_PATH
+export SteamAppId=892970
+
+LOGFILE=/valheim-data/server.log
+LOGFILEARCHIVE="/valheim-data/log_archive/server_$(date +'%Y-%m-%d_%H-%M').log"
+
+mkdir -p /valheim-data/log_archive
+/valheim-server/valheim_server.x86_64 -nographics -batchmode -name "$SERVER_NAME" -port "$SERVER_PORT" -world "$SERVER_WORLD" -password "$SERVER_PASSWORD" -savedir "$DATADIR" -logfile "$LOGFILE"
+cp "$LOGFILE" "$LOGFILEARCHIVE"
+
+export LD_LIBRARY_PATH=$temp_ldpath
